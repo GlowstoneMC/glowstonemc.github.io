5f2bc747562c299812608f0baf37f6d185b0ef6a
mastercoms <mastercoms@tuta.io>
2021-05-27 15:37:42 -0400

    use github.io URL for now

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://glowstone.net'
+SITEURL = 'https://glowstonemc.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/atom/news'
