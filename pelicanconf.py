8f60744061ff9a7b1e029a9ccec8bea0d1a8331e
mastercoms <mastercoms@tuta.io>
2021-06-30 13:08:46 -0400

    update siteurl

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,7 +17,7 @@ ARTICLE_URL = 'news/{slug}/'
 ARTICLE_SAVE_AS = 'news/{slug}/index.html'
 
 STATIC_PATHS = ['images/favicon.ico', 'jd', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'images/favicon.ico': {'path': 'favicon.ico'},}
+EXTRA_PATH_METADATA = {'images/favicon.ico': {'path': 'favicon.ico'}, 'extra/CNAME': {'path': 'CNAME'}, 'extra/CNAME': {'path': 'CNAME'},}
 
 TIMEZONE = 'UTC'
 
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://glowstonemc.github.io'
+SITEURL = 'https://glowstone.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/atom/news'
