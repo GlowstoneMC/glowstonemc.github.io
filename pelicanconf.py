64525552e008b4df4824abbce0198a2f0dcc135c
mastercoms <mastercoms@tuta.io>
2021-06-30 22:51:17 -0400

    move favicon to extra

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,8 +16,8 @@ PAGE_SAVE_AS = '{slug}/index.html'
 ARTICLE_URL = 'news/{slug}/'
 ARTICLE_SAVE_AS = 'news/{slug}/index.html'
 
-STATIC_PATHS = ['images', 'favicon/favicon.ico', 'jd', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'favicon/favicon.ico': {'path': 'favicon.ico'}, 'extra/CNAME': {'path': 'CNAME'},}
+STATIC_PATHS = ['images', 'jd', 'extra/favicon.ico', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'}, 'extra/CNAME': {'path': 'CNAME'},}
 
 TIMEZONE = 'UTC'
 
